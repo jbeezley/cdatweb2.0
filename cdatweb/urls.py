76dccbe30094203b88a01185abc6b5d1697adfed
Matthew Harris <mattbenharris@gmail.com>
2016-03-23 08:38:35 -0700

    MBH: jon B please fix

--- cdatweb/urls.py
+++ cdatweb/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # Pages
     url(r'^$', views.index, name='index'),
     url(r'^login/?$', views.user_login, name='login'),
     url(r'^logout/?$', views.user_logout, name='logout'),
     url(r'^register/?$', views.register, name='register'),
+    # AJAX
     url(r'^get_folder/', views.get_folder),
     url(r'^get_file/', views.get_file),
+    url(r'^get_children', views.get_children),
     url(r'^esgf_search/', views.esgf_search),
+    # VTK
     url(r'^vtk/', views.vtkweb_launcher),
 ]
--- cdatweb/views.py
+++ cdatweb/views.py
@@ -18,7 +18,98 @@ except ImportError:
         "Choose a variable from the list of files available on the server "
         "and drag it to the canvas."
         )
+def render_template(request, template, context):
+    template = loader.get_template(template)
+    context = RequestContext(request, context)
+    return template.render(context)
+       
+# *****
+# CDATWeb Frontend Pages
+# ******
+def index(request):
+    data = {}
+    data['base'] = base_path
+
+    data['files'] = [
+        f for f in os.listdir(base_path)
+        if not os.path.isdir(os.path.join(base_path, f))
+    ]
+    data['dirs'] = [
+        f for f in os.listdir(base_path)
+        if os.path.isdir(os.path.join(base_path, f))
+    ]
+    return HttpResponse(render_template(request, "cdatweb/dashboard.html", data))
+
+def user_login(request):
+    return HttpResponse("login")
+
+def user_logout(request):
+    return HttpResponse("logout")
+
+def register(request):
+    return HttpResponse("register")
+
+# *****
+# CDATWeb Frontend AJAX
+# ******
+@csrf_exempt 
+def esgf_search(request):
+    try:
+        results = {}
+        inputstring = request.POST.get('query')
+        context = json.loads(inputstring)
+
+        host = context["host"]
+        print host
+        query = {}
+        if context["text"]:
+            query["query"] = context["text"]
+        if context["project"]:
+            query["project"] = context["project"]
+        if context["limit"]:
+            query["limit"] = context["limit"]
+        if context["offset"]:
+            query["offset"] = context["offset"]
+        # query['fields'] = 'size,timestamp,project,id,experiment,title,url'
+
+        try:
+            results['data'] = files(host, query)
+        except Exception, e:
+            results['data'] = "failed to reach node"
+            print "failed to reach node"
+            print e
+    except Exception:
+        results['data'] = "failed to parse json"
+        print "failed to parse json"
+
+    return HttpResponse(json.dumps(results))
+    
+@csrf_exempt
+def get_children(request):
+    query = {}
+    folder_content = []
+
+    inputstring = request.POST.get('query')
+    context = json.loads(inputstring)
+
+    path = context["path"]
+
+    for newpath in os.listdir(path):
+        folder_content.append(os.path.join(path, newpath))
+
+    query['files'] = [f for f in folder_content if not os.path.isdir(f)]
+    query['dirs'] = [f for f in folder_content if os.path.isdir(f)]
+    return HttpResponse(json.dumps(query))    
+
+def get_folder(request):
+    return HttpResponse("get folder")
+
+def get_file(request):
+    return HttpResponse("get file")
+
+# ******
 # VTK
+# ******
 def _refresh(request):
     """Refresh the visualization session information."""
     # check the session for a vtkweb instance
@@ -69,92 +160,3 @@ def vtkweb_launcher(request):
             return HttpResponse(status=500)
     return HttpResponse(status=404)
 
-# CDATWeb Frontend
-def render_template(request, template, context):
-    template = loader.get_template(template)
-    context = RequestContext(request, context)
-    return template.render(context)
-
-def index(request):
-    data = {}
-    data['base'] = base_path
-
-    data['files'] = [
-        f for f in os.listdir(base_path)
-        if not os.path.isdir(os.path.join(base_path, f))
-    ]
-    data['dirs'] = [
-        f for f in os.listdir(base_path)
-        if os.path.isdir(os.path.join(base_path, f))
-    ]
-    return HttpResponse(render_template(request, "cdatweb/dashboard.html", data))
-
-def user_login(request):
-    return HttpResponse("login")
-
-def user_logout(request):
-    return HttpResponse("logout")
-
-def register(request):
-    return HttpResponse("register")
-@csrf_exempt 
-def esgf_search(request):
-    try:
-        results = {}
-        inputstring = request.POST.get('query')
-        context = json.loads(inputstring)
-
-        host = context["host"]
-        print host
-        query = {}
-        if context["text"]:
-            query["query"] = context["text"]
-        if context["project"]:
-            query["project"] = context["project"]
-        if context["limit"]:
-            query["limit"] = context["limit"]
-        if context["offset"]:
-            query["offset"] = context["offset"]
-        # query['fields'] = 'size,timestamp,project,id,experiment,title,url'
-
-        try:
-            results['data'] = files(host, query)
-        except Exception, e:
-            results['data'] = "failed to reach node"
-            print "failed to reach node"
-            print e
-    except Exception:
-        results['data'] = "failed to parse json"
-        print "failed to parse json"
-
-    return HttpResponse(json.dumps(results))
-
-def get_folder(request):
-    return HttpResponse("get folder")
-
-def get_file(request):
-    return HttpResponse("get file")
-
-@csrf_exempt
-def vtkweb_launcher(request):
-    """Proxy requests to the configured launcher service."""
-    import requests
-    try:
-        from django.conf import settings
-        VISUALIZATION_LAUNCHER = settings.VISUALIZATION_LAUNCHER
-    except ImportError:
-        VISUALIZATION_LAUNCHER = None
-
-    if not VISUALIZATION_LAUNCHER:
-        # unconfigured launcher
-        return HttpResponse(status=404)
-
-    # TODO: add status and delete methods
-    if request.method == 'POST':
-        req = requests.post(VISUALIZATION_LAUNCHER, request.body)
-        if req.ok:
-            return HttpResponse(req.content)
-        else:
-            return HttpResponse(status=500)
-
-    return HttpResponse(status=404)
